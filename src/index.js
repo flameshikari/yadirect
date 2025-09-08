dbdcba94a3050e056d81b53ac2a2453f1ab20c11
flameshikari <mail@hexed.pw>
2025-09-08 08:48:07 +0500

    add directory listing with /list prefix

--- src/index.js
+++ src/index.js
@@ -15,6 +15,28 @@ const fetch = NodeFetchCache.create({
     shouldCacheResponse: (response) => response.ok,
 });
 
+router.get(/\/list\/([di])\/(\w+)\/?(.*)?/, async (req, res, next) => {
+  try {
+    const regexp = /\/list\/([di])\/(\w+)\/?(.*)?/;
+    const match = req.originalUrl.match(regexp);
+    const type = match[1];
+    const hash = match[2];
+    const folderPath = match[3] ? `path=/${match[3]}&` : '';
+    const publicKeyUrl = `https://disk.yandex.ru/${type}/${hash}`;
+    const apiUrl = `https://cloud-api.yandex.net/v1/disk/public/resources?${folderPath}public_key=${encodeURIComponent(publicKeyUrl)}`;
+  
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    if (response.ok && data._embedded && data._embedded.items) {
+      res.json(data._embedded.items);
+    } else {
+      res.status(response.status).json(data);
+    }
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
 
 router.get(/([di])\/(\w+)\/?(.*)?/, async (req, res, next) => {
     try {
@@ -40,7 +62,6 @@ router.get(/([di])\/(\w+)\/?(.*)?/, async (req, res, next) => {
 router.all('*', (req, res) => {
     res.redirect('/');
 });
-
 
 app.set('json spaces', 2);
 app.set('x-powered-by', false);
